5c972884e1f1494392bfb1a809f7b24f4fd4f9c7
Nithish199 <91787383+Nithish199@users.noreply.github.com>
2021-10-18 18:38:36 +0530

    Add files via upload

---
+++ Acme.java
@@ -0,0 +1,26 @@
+package Week2.Day2.Assignments;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Acme {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://acme-test.uipath.com/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.id("email")).sendKeys("kumar.testleaf@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("leaf@12");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		System.out.println("The page title is " + driver.getTitle());
+		driver.findElement(By.linkText("Log Out")).click();
+		driver.close();
+	}
+
+}
